bc2c3978c0850fae892a61ca9d836bf01a29c1ca
Emil1991 <emilgulayev@gmail.com>
2019-12-08 08:57:36 +0000

    malloc1

--- malloc_1.cpp
+++ malloc_1.cpp
@@ -1,5 +1,13 @@
 #include <cstdlib>
+#include <cmath>
+#include <iostream>
 
 void* smalloc(size_t size){
+    if(size==0 || size>pow(10,8))
+        return NULL;
+    void* currPB=sbrk(0);
+    if(sbrk(size)!=currPB)
+        return NULL;
 
+    std::cout << "allocated: "<< size << std::endl;
 }
